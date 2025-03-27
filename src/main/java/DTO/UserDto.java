e4726a282f377292e167608317c41cfae150306a
Andreas Gabel <andreassgabel@hotmail.com>
2025-03-27 10:59:17 +0100

    issue 4, oprettet userDto, userController, UserService til brugeroprettelse

---
+++ src/main/java/DTO/UserDto.java
@@ -0,0 +1,31 @@
+package DTO;
+
+//Data transfer object - Vi skal transporterer noget data mellem flere klasser (god dammeldags objekt konstruktor klasse)
+public class UserDto {
+
+    private String name;
+    private String email;
+    private String password;
+
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
