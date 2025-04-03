f70a6af22018d08e6244b95467b4a95ad75e0a99
Andreas Gabel <andreassgabel@hotmail.com>
2025-04-03 12:27:33 +0200

    Andreas har lavet noget backend, frontend Cookies, datapolitik, om os

---
+++ src/main/java/com/example/wishlist/Controller/InfoPageController.java
@@ -0,0 +1,24 @@
+package com.example.wishlist.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class InfoPageController {
+
+    @GetMapping("/cookies")
+    public String showCookiePolicy() {
+        return "cookies"; // viser cookies.html
+    }
+
+    @GetMapping("/privacy")
+    public String showPrivacyPolicy() {
+        return "privacy"; // viser privacy.html
+    }
+
+    @GetMapping("/about")
+    public String showAboutUsPage() {
+        return "about-us"; // loader about-us.html fra /templates
+    }
+
+}
